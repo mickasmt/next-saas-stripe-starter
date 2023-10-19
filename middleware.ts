d2a65ebb54433bf2ed43fef08ca1c0c1c0ae843e
mickasmt <sermont.mickael@hotmail.fr>
2023-10-19 23:05:28 +0200

    feat: remove editor

--- middleware.ts
+++ middleware.ts
@@ -42,5 +42,5 @@ export default withAuth(
 )
 
 export const config = {
-  matcher: ["/dashboard/:path*", "/editor/:path*", "/login", "/register"],
+  matcher: ["/dashboard/:path*", "/login", "/register"],
 }
