e10f7764fb731e3488258a062672216293ebb077
mickasmt <sermont.mickael@hotmail.fr>
2023-10-24 00:48:54 +0200

    feat: update home logo section

--- config/marketing.ts
+++ config/marketing.ts
@@ -3,10 +3,6 @@ import { MarketingConfig } from "types"
 export const marketingConfig: MarketingConfig = {
   mainNav: [
     {
-      title: "Features",
-      href: "/#features",
-    },
-    {
       title: "Pricing",
       href: "/pricing",
     },
