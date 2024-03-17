43c998ea13eb7aba51ab6e8385a63ac0f3cff97b
mickasmt <sermont.mickael@hotmail.fr>
2024-03-17 12:55:11 +0100

    refactor: update next-themes v3 & remove providers

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,11 @@ import "@/styles/globals.css";
 import { fontHeading, fontSans, fontUrban } from "@/assets/fonts";
 import { Analytics } from "@/components/analytics";
 import { ModalProvider } from "@/components/modal-provider";
-import { ThemeProvider } from "@/components/providers";
 import { TailwindIndicator } from "@/components/tailwind-indicator";
 import { Toaster } from "@/components/ui/toaster";
 import { siteConfig } from "@/config/site";
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "next-themes";
 
 interface RootLayoutProps {
   children: React.ReactNode
