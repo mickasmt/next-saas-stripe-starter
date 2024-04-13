74060e03fb5a5263998cc7dbd607104058fd708e
mickasmt <sermont.mickael@hotmail.fr>
2024-04-13 07:40:22 +0200

    refactor: update stripe apiVersion

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -3,6 +3,6 @@ import Stripe from "stripe"
 import { env } from "@/env.mjs"
 
 export const stripe = new Stripe(env.STRIPE_API_KEY, {
-  apiVersion: "2023-10-16",
+  apiVersion: "2024-04-10",
   typescript: true,
 })
