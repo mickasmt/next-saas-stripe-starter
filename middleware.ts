cbcf0a21c87314320e831611d3e4e437eec577d3
mickasmt <sermont.mickael@hotmail.fr>
2024-05-06 16:57:51 +0200

    fix: update regex matcher

--- middleware.ts
+++ middleware.ts
@@ -1,59 +1,59 @@
-import NextAuth, { Session } from "next-auth"
-import { NextRequest } from 'next/server';
-
-import authConfig from "./auth.config"
+import { NextRequest } from "next/server";
 import {
-  DEFAULT_LOGIN_REDIRECT,
   apiAuthPrefix,
   authRoutes,
+  DEFAULT_LOGIN_REDIRECT,
   publicRoutes,
 } from "@/routes";
+import NextAuth, { Session } from "next-auth";
 
-const { auth: middleware } = NextAuth(authConfig)
+import authConfig from "./auth.config";
 
-export default middleware((req: NextRequest & { auth: Session | null }): Response | void  => {
-  const { nextUrl } = req;
-  const isLoggedIn = !!req.auth;
+const { auth: middleware } = NextAuth(authConfig);
 
-  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
-  const isPublicRoute = publicRoutes.some(route => {
-    if (route === "/") {
-      return nextUrl.pathname === route;
-    } else {
-      return nextUrl.pathname.startsWith(route);
+export default middleware(
+  (req: NextRequest & { auth: Session | null }): Response | void => {
+    const { nextUrl } = req;
+    const isLoggedIn = !!req.auth;
+
+    const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+    const isPublicRoute = publicRoutes.some((route) => {
+      if (route === "/") {
+        return nextUrl.pathname === route;
+      } else {
+        return nextUrl.pathname.startsWith(route);
+      }
+    });
+
+    const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+    if (isApiAuthRoute) return;
+
+    if (isAuthRoute) {
+      if (isLoggedIn) {
+        return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+      }
+      return;
     }
-  });
 
-  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+    if (!isLoggedIn && !isPublicRoute) {
+      let callbackUrl = nextUrl.pathname;
+      if (nextUrl.search) {
+        callbackUrl += nextUrl.search;
+      }
 
-  if (isApiAuthRoute) return;
+      const encodedCallbackUrl = encodeURIComponent(callbackUrl);
 
-  if (isAuthRoute) {
-    if (isLoggedIn) {
-      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
+      return Response.redirect(
+        new URL(`/login?callbackUrl=${encodedCallbackUrl}`, nextUrl),
+      );
     }
+
     return;
-  }
-
-  if (!isLoggedIn && !isPublicRoute) {
-    let callbackUrl = nextUrl.pathname;
-    if (nextUrl.search) {
-      callbackUrl += nextUrl.search;
-    }
-
-    const encodedCallbackUrl = encodeURIComponent(callbackUrl);
-
-    return Response.redirect(new URL(
-      `/login?callbackUrl=${encodedCallbackUrl}`,
-      nextUrl
-    ));
-  }
-
-  return;
-})
+  },
+);
 
 // Optionally, don't invoke Middleware on some paths
 export const config = {
-  // clerck matcher
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-}
+  matcher: ["/((?!.+\\.[\\w]+$|_next|api).*)", "/"],
+};
