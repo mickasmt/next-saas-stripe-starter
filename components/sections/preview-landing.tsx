e59a5b19554530e9fe0374caa31161a741f7fcd0
mickasmt <sermont.mickael@hotmail.fr>
2024-06-09 23:13:24 +0200

    fix: inverted preview img in dark mode

--- components/sections/preview-landing.tsx
+++ components/sections/preview-landing.tsx
@@ -9,7 +9,7 @@ export default function PreviewLanding() {
         <div className="rounded-xl md:bg-muted/30 md:p-3.5 md:ring-1 md:ring-inset md:ring-border">
           <div className="relative aspect-video overflow-hidden rounded-xl border md:rounded-lg">
             <Image
-              className="size-full object-cover object-center"
+              className="size-full object-cover object-center dark:opacity-85 dark:invert"
               src="/images/blog/blog-post-3.jpg"
               alt="preview landing"
               width={2000}
