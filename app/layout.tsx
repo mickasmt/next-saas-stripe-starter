e1827488cd53269c654f5b052721d09f9145367f
logiserv <59137335+logiserv@users.noreply.github.com>
2025-04-12 18:33:00 +0400

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,8 @@
-"use client"; // Add this at the top to mark it as a Client Component
-
 import "@/styles/globals.css";
 
 import { fontGeist, fontHeading, fontSans, fontUrban } from "@/assets/fonts";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
-import { usePathname } from "next/navigation";
 
 import { cn, constructMetadata } from "@/lib/utils";
 import { Toaster } from "@/components/ui/sonner";
@@ -20,8 +17,6 @@ interface RootLayoutProps {
 export const metadata = constructMetadata();
 
 export default function RootLayout({ children }: RootLayoutProps) {
-  const pathname = usePathname(); // Hook that requires a Client Component
-
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
@@ -41,15 +36,6 @@ export default function RootLayout({ children }: RootLayoutProps) {
             enableSystem
             disableTransitionOnChange
           >
-            <div className="language-switcher" style={{ position: "absolute", top: 10, right: 10 }}>
-              <select
-                onChange={(e) => console.log(`Language selected: ${e.target.value}`)}
-                className="p-2 bg-white border border-gray-300 rounded"
-              >
-                <option value="en">English</option>
-                <option value="fr">Français</option>
-              </select>
-            </div>
             <ModalProvider>{children}</ModalProvider>
             <Analytics />
             <Toaster richColors closeButton />
