27df78fc5d2f1b351f90d0047a8cb463a32eb238
logiserv <59137335+logiserv@users.noreply.github.com>
2025-04-12 22:42:25 +0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import { fontGeist, fontHeading, fontSans, fontUrban } from "@/assets/fonts";
 import { SessionProvider } from "next-auth/react";
@@ -38,6 +39,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
           >
             <ModalProvider>{children}</ModalProvider>
             <Analytics />
+            <SpeedInsights />
             <Toaster richColors closeButton />
             <TailwindIndicator />
           </ThemeProvider>
