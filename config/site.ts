c5ef52b3bff37ec80e55be4ab1da12106144c198
logiserv <59137335+logiserv@users.noreply.github.com>
2025-04-12 23:09:38 +0400

    Update site.ts

--- config/site.ts
+++ config/site.ts
@@ -4,7 +4,7 @@ import { env } from "@/env.mjs";
 const site_url = env.NEXT_PUBLIC_APP_URL;
 
 export const siteConfig: SiteConfig = {
-  name: "SaaS Starter",
+  name: "Logiserv",
   description:
     "Get your project off to an explosive start with SaaS Starter! Harness the power of Next.js 14, Prisma, Neon, Auth.js v5, Resend, React Email, Shadcn/ui and Stripe to build your next big thing.",
   url: site_url,
