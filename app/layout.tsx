19e38640e12327d73c9319a4c2f3a7a6bebd247c
logiserv <59137335+logiserv@users.noreply.github.com>
2025-04-12 18:12:32 +0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import "@/styles/globals.css";
 import { fontGeist, fontHeading, fontSans, fontUrban } from "@/assets/fonts";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
-import { useRouter } from "next/router";
 
 import { cn, constructMetadata } from "@/lib/utils";
 import { Toaster } from "@/components/ui/sonner";
@@ -18,15 +17,8 @@ interface RootLayoutProps {
 export const metadata = constructMetadata();
 
 export default function RootLayout({ children }: RootLayoutProps) {
-  const router = useRouter();
-  const { locale, pathname, query, asPath } = router;
-
-  const changeLanguage = (lang: string) => {
-    router.push({ pathname, query }, asPath, { locale: lang });
-  };
-
   return (
-    <html lang={locale || "en"} suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning>
       <head />
       <body
         className={cn(
@@ -44,22 +36,6 @@ export default function RootLayout({ children }: RootLayoutProps) {
             enableSystem
             disableTransitionOnChange
           >
-            <div className="language-switcher" style={{ position: "absolute", top: 10, right: 10 }}>
-              <button
-                onClick={() => changeLanguage("en")}
-                disabled={locale === "en"}
-                className={cn("p-2", locale === "en" ? "text-gray-500" : "text-black")}
-              >
-                English
-              </button>
-              <button
-                onClick={() => changeLanguage("fr")}
-                disabled={locale === "fr"}
-                className={cn("p-2", locale === "fr" ? "text-gray-500" : "text-black")}
-              >
-                Français
-              </button>
-            </div>
             <ModalProvider>{children}</ModalProvider>
             <Analytics />
             <Toaster richColors closeButton />
