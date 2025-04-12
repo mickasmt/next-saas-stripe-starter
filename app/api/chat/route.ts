269fb2886f0a34e197a5e1df189b495ed5fbf290
logiserv <59137335+logiserv@users.noreply.github.com>
2025-04-13 00:06:39 +0400

    Create route.ts

---
+++ app/api/chat/route.ts
@@ -0,0 +1,26 @@
+// app/api/chat/route.ts
+import { createOpenAI } from '@ai-sdk/openai';
+import { streamText } from 'ai';
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
+const perplexity = createOpenAI({
+  name: 'perplexity',
+  apiKey: process.env.PERPLEXITY_API_KEY ?? '',
+  baseURL: 'https://api.perplexity.ai/',
+});
+
+export async function POST(req: Request) {
+  // Extract the `messages` from the body of the request
+  const { messages } = await req.json();
+
+  // Call the language model
+  const result = streamText({
+    model: perplexity('llama-3.1-sonar-large-32k-online'),
+    messages,
+  });
+
+  // Respond with the stream
+  return result.toDataStreamResponse();
+}
