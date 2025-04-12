f9091fb2a4a2638bd18af2e6c69e5363f511f23e
logiserv <59137335+logiserv@users.noreply.github.com>
2025-04-12 23:08:44 +0400

    Update marketing.ts

--- config/marketing.ts
+++ config/marketing.ts
@@ -3,7 +3,7 @@ import { MarketingConfig } from "types"
 export const marketingConfig: MarketingConfig = {
   mainNav: [
     {
-      title: "Pricing",
+      title: "Tarifs",
       href: "/pricing",
     },
     {
